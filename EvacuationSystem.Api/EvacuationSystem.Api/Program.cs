679831ade6143df7a64a4cf995f6cb07b8242997
Horetskyi Maksym <s1a6ks@gmail.com>
2025-12-02 15:42:23 +0200

    test commit

--- EvacuationSystem.Api/EvacuationSystem.Api/Program.cs
+++ EvacuationSystem.Api/EvacuationSystem.Api/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -9,7 +9,6 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
